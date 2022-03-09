b04e982fb12a3d2f2001223252d88695b4832b83
Philippe <philippe.vial90@gmail.com>
2022-03-09 14:37:50 +0100

    Initial Commit

---
+++ src/main/java/se/iths/corkdork/CorkDorkApplication.java
@@ -0,0 +1,13 @@
+package se.iths.corkdork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorkDorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorkDorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/corkdork/CorkDorkApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.corkdork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorkDorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

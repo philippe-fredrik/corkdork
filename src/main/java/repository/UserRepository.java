3fe50da257a4bc83261a7a047b31ef9637b1572c
Philippe <philippe.vial90@gmail.com>
2022-03-14 09:08:58 +0100

    Delete get method for field that don't exist(username).

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -6,7 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
-    UserEntity findbyUsername(String username);
-
-
 }

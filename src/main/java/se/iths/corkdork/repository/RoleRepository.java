ae2868d597709cdda5ead237807f39f2f92a60dc
Philippe <philippe.vial90@gmail.com>
2022-03-14 10:36:11 +0100

    Add RoleEntity

---
+++ src/main/java/se/iths/corkdork/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package se.iths.corkdork.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import se.iths.corkdork.entity.RoleEntity;
+
+@Repository
+public interface RoleRepository extends CrudRepository<RoleEntity, Long> {
+}

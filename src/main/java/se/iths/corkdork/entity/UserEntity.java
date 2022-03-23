99ee963800b0f4caf9df5e48c392d2d4c4a3acc4
Philippe <philippe.vial90@gmail.com>
2022-03-23 10:42:08 +0100

    Add not @Column(nullable = false) to fields

--- src/main/java/se/iths/corkdork/entity/UserEntity.java
+++ src/main/java/se/iths/corkdork/entity/UserEntity.java
@@ -13,10 +13,15 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
-    private String userName;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false, unique = true)
     private String email;
 
     @ManyToOne

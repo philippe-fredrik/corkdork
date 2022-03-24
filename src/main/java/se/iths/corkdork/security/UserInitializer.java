bb47e7a0cca48431789e416c48708b37037f9eba
Philippe <philippe.vial90@gmail.com>
2022-03-24 14:13:24 +0100

    Refactor the initialization of user data on start

--- src/main/java/se/iths/corkdork/security/UserInitializer.java
+++ src/main/java/se/iths/corkdork/security/UserInitializer.java
@@ -1,6 +1,7 @@
 package se.iths.corkdork.security;
 
 
+import org.apache.catalina.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,14 @@ import se.iths.corkdork.entity.UserEntity;
 import se.iths.corkdork.repository.RoleRepository;
 import se.iths.corkdork.repository.UserRepository;
 
+import java.util.List;
+
 @Service
-public class UserInitializer implements CommandLineRunner{
+public class UserInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final RoleRepository roleRepository;
-
 
     public UserInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
@@ -23,20 +25,31 @@ public class UserInitializer implements CommandLineRunner{
         this.roleRepository = roleRepository;
     }
 
-
     @Override
     public void run(String... args) {
         userRepository.deleteAll();
         roleRepository.deleteAll();
+
+        RoleEntity adminRole = new RoleEntity("ADMIN");
+        RoleEntity userRole = new RoleEntity("USER");
+
+        UserEntity user = new UserEntity(
+                "Test",
+                "Test",
+                "User",
+                passwordEncoder.encode("user123"),
+                "user@corkdork.se",
+                userRole);
+
         UserEntity admin = new UserEntity(
                 "Test",
                 "Test",
                 "Admin",
                 passwordEncoder.encode("admin123"),
-                "admin@corkdork.se");
-        RoleEntity adminRole = new RoleEntity("ROLE_ADMIN");
-        admin.setRole(adminRole);
-        this.roleRepository.save(adminRole);
-        this.userRepository.save(admin);
+                "admin@corkdork.se",
+                adminRole);
+
+        this.roleRepository.saveAll(List.of(adminRole, userRole));
+        this.userRepository.saveAll(List.of(admin, user));
     }
 }

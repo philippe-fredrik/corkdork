733a3b7d01252fe9965627d20e0591d5c283be8d
Philippe <philippe.vial90@gmail.com>
2022-03-25 21:05:19 +0100

    Refactor antmatchers + add urls for ignoringAntMatchers

--- src/main/java/se/iths/corkdork/security/SecurityConfig.java
+++ src/main/java/se/iths/corkdork/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package se.iths.corkdork.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -16,23 +15,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final String[] urls = {"/users/**", "/users/signup","/wines/**", "/roles", "/countries", "/grapes"};
+
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
+                .csrf().ignoringAntMatchers(urls)
+                .and()
                 .httpBasic()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/home", "/users/signup").permitAll()
-                .antMatchers("/users", "/users/").hasRole("USER")
+                .antMatchers("/**/admin/**", "/**/admin").hasRole("ADMIN")
+                .antMatchers("/**/public/**", "/**/public").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
                 .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
                 .and().build();
     }
 

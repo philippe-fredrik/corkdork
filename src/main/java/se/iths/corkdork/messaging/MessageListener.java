c507b0ab3a349844e5c37005a46a91e3d45902b4
Philippe <philippe.vial90@gmail.com>
2022-03-30 12:43:34 +0200

    Add MessageListener

---
+++ src/main/java/se/iths/corkdork/messaging/MessageListener.java
@@ -0,0 +1,13 @@
+package se.iths.corkdork.messaging;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void listener(CustomMessage message) {
+        System.out.println(message);
+    }
+}

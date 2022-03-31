575eb9f74c2e4a89835d22d463b6055f534bae9a
Philippe <philippe.vial90@gmail.com>
2022-03-31 11:58:31 +0200

    Add findByName to repos

--- src/main/java/se/iths/corkdork/repository/CountryRepository.java
+++ src/main/java/se/iths/corkdork/repository/CountryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CountryRepository extends CrudRepository<CountryEntity, Long> {
-
+    CountryEntity findByCountryName(String name);
 }

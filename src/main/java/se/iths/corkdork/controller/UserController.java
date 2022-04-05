c25d3f473c41e82f03c97b45e211188e67706133
Philippe <philippe.vial90@gmail.com>
2022-04-05 15:02:44 +0200

    Add MessagePublisher to create request

--- src/main/java/se/iths/corkdork/controller/UserController.java
+++ src/main/java/se/iths/corkdork/controller/UserController.java
@@ -6,8 +6,8 @@ import se.iths.corkdork.dtos.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import se.iths.corkdork.exception.BadRequestException;
 import se.iths.corkdork.exception.EntityNotFoundException;
+import se.iths.corkdork.messaging.MessagePublisher;
 import se.iths.corkdork.service.UserService;
 
 @RestController
@@ -15,19 +15,21 @@ import se.iths.corkdork.service.UserService;
 public class UserController {
 
     private final UserService userService;
+    private final MessagePublisher messagePublisher;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, MessagePublisher messagePublisher) {
         this.userService = userService;
+
+        this.messagePublisher = messagePublisher;
     }
 
     @PostMapping("signup")
     public ResponseEntity<User> createUser(@Validated @RequestBody User user, BindingResult errors) {
-
         if (errors.hasErrors())
             throw new BadRequestException("Invalid input", errors);
 
         User createdUser = userService.createUser(user);
-
+        messagePublisher.sendMessage(createdUser.getUsername());
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
@@ -55,7 +57,7 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-  
+
     @GetMapping("")
     public ResponseEntity<Iterable<User>> findAllUsers() {
 
